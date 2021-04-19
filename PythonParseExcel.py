e99477960d5a43bb6db8a9fde0104f09eb9de4a9
CyberCyber2 <56095531+CyberCyber2@users.noreply.github.com>
2021-04-19 17:23:44 -0400

    Update PythonParseExcel.py

--- PythonParseExcel.py
+++ PythonParseExcel.py
@@ -1,1 +1,12 @@
-
+import xlrd
+###############
+stringToFind = "linux"
+ExcelDocument = "mySpreadSheet.xls"
+###############
+mySheet = xlrd.open_workbook(ExcelDocument)
+for s in mySheet.sheets():
+	for rowidx in range(sheet.nrows):
+		row = sheet.row(rowidx)
+		for colidx, cell in enumerate(row):
+			if cell.value == stringToFind:
+				print (str(sheet.name) + " is located at: " + (colidx) + "," (rowidx))
